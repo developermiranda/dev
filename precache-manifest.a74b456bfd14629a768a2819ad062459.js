b9d73dcf183291ab03d100f706389c4506c630df
Daniel Miranda <developermiranda@gmail.com>
2020-04-11 20:01:43 -0300

    Updates

---
+++ precache-manifest.a74b456bfd14629a768a2819ad062459.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fec58acdf20f5ccc666cc3677365f61",
+    "url": "/dev/index.html"
+  },
+  {
+    "revision": "7445620c1f9675779c97",
+    "url": "/dev/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91e39f1b3c679b0c078f",
+    "url": "/dev/static/js/2.b714eae3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dev/static/js/2.b714eae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7445620c1f9675779c97",
+    "url": "/dev/static/js/main.1dc257f1.chunk.js"
+  },
+  {
+    "revision": "77fcf533ce8b93fc63d5",
+    "url": "/dev/static/js/runtime-main.106c67e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
